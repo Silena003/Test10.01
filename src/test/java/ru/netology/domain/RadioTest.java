7c07783444c1608b35684b2f8bd8c95454a50106
Elena Ryzhkina <silena003@gmail.com>
2022-03-08 14:29:29 +0300

    Initial commit

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -40,7 +40,34 @@ class RadioTest {
         assertEquals(0, radio.getCurrentVolume());
 
     }
-
+    @Test
+    public void threeVolume() {
+        radio.setMaxCurrentVolume(10);
+        radio.setMinCurrentVolume(0);
+        radio.setCurrentVolume(1);
+        assertEquals(2, radio.getCurrentVolume());
+    }
+    @Test
+    public void fourVolume() {
+        radio.setMaxCurrentVolume(10);
+        radio.setMinCurrentVolume(0);
+        radio.setCurrentVolume(2);
+        assertEquals(3, radio.getCurrentVolume());
+    }
+    @Test
+    public void fiveVolume() {
+        radio.setMaxCurrentVolume(10);
+        radio.setMinCurrentVolume(0);
+        radio.setCurrentVolume(3);
+        assertEquals(4, radio.getCurrentVolume());
+    }
+    @Test
+    public void sixVolume() {
+        radio.setMaxCurrentVolume(10);
+        radio.setMinCurrentVolume(0);
+        radio.setCurrentVolume(4);
+        assertEquals(5, radio.getCurrentVolume());
+    }
 
     @Test
     public void setIncreaseRadioStation() {
@@ -60,15 +87,43 @@ class RadioTest {
     public void oneRadioStation() {
         radio.setMaxRadioStation(9);
         radio.setMinRadioStation(0);
-        radio.setCurrentRadioStation(7);
-        assertEquals(7, radio.getCurrentRadioStation());
+        radio.setCurrentRadioStation(3);
+        assertEquals(3, radio.getCurrentRadioStation());
     }
     @Test
     public void twoRadioStation() {
         radio.setMaxRadioStation(9);
         radio.setMinRadioStation(0);
+        radio.setCurrentRadioStation(5);
+        assertEquals(5, radio.getCurrentRadioStation());
+    }
+    @Test
+    public void sixRadioStation() {
+        radio.setMaxRadioStation(9);
+        radio.setMinRadioStation(0);
+        radio.setCurrentRadioStation(7);
+        assertEquals(7, radio.getCurrentRadioStation());
+    }
+    @Test
+    public void threeRadioStation() {
+        radio.setMaxRadioStation(9);
+        radio.setMinRadioStation(0);
         radio.setCurrentRadioStation(6);
         assertEquals(6, radio.getCurrentRadioStation());
+    }
+    @Test
+    public void fourRadioStation() {
+        radio.setMaxRadioStation(9);
+        radio.setMinRadioStation(0);
+        radio.setCurrentRadioStation(2);
+        assertEquals(2, radio.getCurrentRadioStation());
+    }
+    @Test
+    public void fiveRadioStation() {
+        radio.setMaxRadioStation(9);
+        radio.setMinRadioStation(0);
+        radio.setCurrentRadioStation(1);
+        assertEquals(1, radio.getCurrentRadioStation());
     }
 
 //    @Test
